98b4d49b9946e7130fe0d4f60baddf9d92244b31
Danielle Pham <quelledanielle@gmail.com>
2013-07-04 14:08:12 -0400

    Defined functions and ARGS class

--- analyze.py
+++ analyze.py
@@ -2,6 +2,25 @@
 
 import sys
 
+MAX_ARRAY_SIZE = 250000000
+
+class ARGS(object):
+    def __init__(self, chromosome, TF1_name, TF1_code, TF2_code):
+        self.chr = chromosome
+        self.TF1_name = TF1_name
+        self.TF1_code = TF1_code
+        self.TF2_code = TF2_code
+    
+    def __repr__(self):
+        return ("%s %s %s %s" % 
+               (self.chr, self.TF1_name, self.TF1_code, self.TF2_code))
+
+def all():
+    pass
+
+def run():
+    pass
+
 def main(argv=sys.argv[1:]):
     pass
 
