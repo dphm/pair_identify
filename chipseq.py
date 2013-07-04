4d6c3138bb8d022a24d56398bb1ad8176c99bdf1
Danielle Pham <quelledanielle@gmail.com>
2013-07-04 13:48:02 -0400

    Set up ChipSeq, RMSK methods

--- chipseq.py
+++ chipseq.py
@@ -1,3 +1,19 @@
 class ChipSeq(object):
-    def __init__(self):
-        pass
+    def __init__(self, chromosome, code):
+        self.code = "chip_seq_" + code[0:6]
+        self.filepath = ("/scratch/dpham4/data/%s/%s.txt" %
+                        (chromosome, self.code))
+    
+    def fill(self, data):
+        with open(self.filepath) as f_in:
+            for l in f_in:
+                line = l.split()
+                
+                try:
+                    start = int(line[1])
+                    finish = int(line[2]) + 1
+                    
+                    for i in xrange(start, finish):
+                        data[i] += 1
+                except ValueError:
+                    continue
--- rmsk.py
+++ rmsk.py
@@ -1,3 +1,17 @@
 class RMSK(object):
-    def __init__(self):
-        pass
+    def __init__(self, chromosome):
+        self.filepath = "/scrach/dpham4/data/%s/rmsk.txt" % chromosome
+    
+    def fill(self, data):
+        with open(self.filepath) as f_in:
+            for l in f_in:
+                line = l.split()
+                
+                try:
+                    start = int(line[6])
+                    finish = int(line[7]) + 1
+                
+                    for i in xrange(start, finish):
+                        data[i] += 10
+                except ValueError:
+                    continue
