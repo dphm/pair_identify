de174b72c0cd82b21c617e6a2fa085752392418a
Danielle Pham <quelledanielle@gmail.com>
2013-07-04 14:15:37 -0400

    Command line processing and args

--- analyze.py
+++ analyze.py
@@ -15,14 +15,35 @@ class ARGS(object):
         return ("%s %s %s %s" % 
                (self.chr, self.TF1_name, self.TF1_code, self.TF2_code))
 
-def all():
+def all(argv):
     pass
 
-def run():
-    pass
+def run(argv):
+    args = ARGS(argv[0], argv[1], argv[2], argv[3])
+    
+    with open("log", "a") as sys.stdout:
+        print "Processing: %s" % args
+        print "Completed: %s" % args
 
 def main(argv=sys.argv[1:]):
-    pass
+    # command line processing
+    arg_len = len(argv)
+    
+    if arg_len == 4:
+        run(argv)
+        return 0
+    elif arg_len == 2:
+        if argv[0] == "--all":
+            all(argv)
+            return 0
+        else:
+            print "usage:   python analyze.py --all CHR"
+            print "example: python analyze.py --all chr1"
+            return 1
+    else:
+        print "usage:   python analyze.py CHR TF1_NAME TF1_CODE TF2_CODE"
+        print "example: python analyze.py chr1 NFKB M00774 M00497"
+        return 1
 
 if __name__ == '__main__':
     status = main()
