dc782a0fc245d6bdc5a9e1e6d9eec4a3403be68d
Danielle Pham <quelledanielle@gmail.com>
2013-07-05 15:07:33 -0400

    Renamed major and minor, exit codes in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 CHR="$1"
-MAJOR_NAME="_$2"
-MAJOR_CODE="$3"
-MINOR_CODE="$4"
+TF1_NAME="_$2"
+TF1_CODE="$3"
+TF2_CODE="$4"
 
 #---------------#
 #  input files  #
 #---------------#
 RMSK_IN="/scratch/dpham4/PI/data/rmsk.txt.gz"
-CHIP_SEQ_IN="/home/mcb/blanchem/wgEncodeRegTfbsClustered/${MAJOR_CODE:0:6}$MAJOR_NAME.bed"
-MAJOR_IN="/scratch/blanchem/$CHR/sites/sites.$MAJOR_CODE.gz"
-MINOR_IN="/scratch/blanchem/$CHR/sites/sites.$MINOR_CODE.gz"
+CHIP_IN="/home/mcb/blanchem/wgEncodeRegTfbsClustered/${TF1_CODE:0:6}$TF1_NAME.bed"
+TF1_IN="/scratch/blanchem/$CHR/sites/sites.$TF1_CODE.gz"
+TF2_IN="/scratch/blanchem/$CHR/sites/sites.$TF2_CODE.gz"
 
 #----------------#
 #  output files  #
 #----------------#
 RMSK_OUT="/scratch/dpham4/PI/data/$CHR/rmsk.txt"
-CHIP_SEQ_OUT="/scratch/dpham4/PI/data/$CHR/chip_seq_${MAJOR_CODE:0:6}.txt"
-MAJOR_OUT="/scratch/dpham4/PI/data/$CHR/$MAJOR_CODE.txt"
-MINOR_OUT="/scratch/dpham4/PI/data/$CHR/$MINOR_CODE.txt"
+CHIP_OUT="/scratch/dpham4/PI/data/$CHR/chip_seq_${TF1_CODE:0:6}.txt"
+TF1_OUT="/scratch/dpham4/PI/data/$CHR/$TF1_CODE.txt"
+TF2_OUT="/scratch/dpham4/PI/data/$CHR/$TF2_CODE.txt"
 
 #--------#
 #  main  #
@@ -34,32 +34,32 @@ then
     fi
 fi
 
-if [ ! -f $CHIP_SEQ_OUT ]
+if [ ! -f $CHIP_OUT ]
 then
-    grep -w $CHR $CHIP_SEQ_IN > $CHIP_SEQ_OUT
+    grep -w $CHR $CHIP_IN > $CHIP_OUT
     if [ "$?" != "0" ]
     then
-        rm -f $CHIP_SEQ_OUT
-        exit 1
+        rm -f $CHIP_OUT
+        exit 2
     fi
 fi
 
-if [ ! -f $MAJOR_OUT ]
+if [ ! -f $TF1_OUT ]
 then
-    zcat $MAJOR_IN | grep -w '^0' > $MAJOR_OUT
+    zcat $TF1_IN | grep -w '^0' > $TF1_OUT
     if [ "$?" != "0" ]
     then
-        rm -f $MAJOR_OUT
-        exit 1
+        rm -f $TF1_OUT
+        exit 3
     fi
 fi
 
-if [ ! -f $MINOR_OUT ]
+if [ ! -f $TF2_OUT ]
 then
-    zcat $MINOR_IN | grep -w '^0' > $MINOR_OUT
+    zcat $TF2_IN | grep -w '^0' > $TF2_OUT
     if [ "$?" != "0" ]
     then
-        rm -f $MINOR_OUT
-        exit 1
+        rm -f $TF2_OUT
+        exit 4
     fi
 fi
