10041daf690b923be33c6e7d603052bb1a0f2af1
Danielle Pham <quelledanielle@gmail.com>
2013-07-15 12:49:04 -0400

    Remove ValueError exception

--- rmsk.py
+++ rmsk.py
@@ -12,13 +12,10 @@ class RMSK(object):
             for l in f_in:
                 line = l.split()
                 
-                try:
-                    start = int(line[0])
-                    finish = int(line[1])
-                
-                    for i in xrange(start, finish):
-                        data.add(i)
-                except ValueError:
-                    continue
+                start = int(line[0])
+                finish = int(line[1])
+            
+                for i in xrange(start, finish):
+                    data.add(i)
         
         self.data = data
