3c2e02af91162bb4a5717a7f754a5054a1a5fb32
Danielle Pham <quelledanielle@gmail.com>
2013-07-17 12:20:44 -0400

    Strip string representation of name

--- analyze.py
+++ analyze.py
@@ -133,12 +133,12 @@ def tf_lists(chromosome):
         # MXXXXX NAME\n
         for entry in list_1:
             code, name = entry.split()
-            tf1_list[code] = name.rstrip()
+            tf1_list[code] = str(name).rstrip()
     
     with open(tf2path) as list_2:
         # MXXXXX\n
         for entry in list_2:
-            name = entry.rstrip()
+            name = str(entry).rstrip()
             tf2_list.append(entry)
     
     return tf1_list, tf2_list
