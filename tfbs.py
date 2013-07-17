ae9d458e6452c75c60a8eff32a69953fa21cc5b7
Danielle Pham <quelledanielle@gmail.com>
2013-07-17 13:07:58 -0400

    Except IndexError in TFBS, problems from original data at last line (e.g. M00671)

--- tfbs.py
+++ tfbs.py
@@ -20,7 +20,7 @@ class TFBS(object):
                     i = int(line[1])
                     if i >= 0:
                         sites.append(i)
-                except ValueError:
+                except ValueError, IndexError:
                     continue
         
         self.sites = sites
